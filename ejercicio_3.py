7015eab539f92b029cd67ab0cc37918a37901f74
JhonnyBravo9696 <jesuscortesnavarro1705@gmail.com>
2025-10-08 08:57:45 +0200

    Create ejercicio_3.py

---
+++ ejercicio_3.py
@@ -0,0 +1,6 @@
+cateto1 = float(input("introduce el primer cateto:"))
+cateto2 = float(input("introduce el segundo cateto:"))
+
+hipotenusa = (cateto1 ** 2 + cateto2 ** 2)
+
+print(f'hiporenusa= {hipotenusa}')
