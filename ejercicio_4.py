f2cf67eb19ca6962c7e505dfb23b2c49087a686d
JhonnyBravo9696 <jesuscortesnavarro1705@gmail.com>
2025-10-09 09:37:35 +0200

    Create ejercicio_4.py

---
+++ ejercicio_4.py
@@ -0,0 +1,12 @@
+Numero1 = float(input("introduce el primer numero:"))
+Numero2 = float(input("introduce el segundo numero:"))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2 
+
+print (f'suma = {suma}')
+print (f'resta = {resta}')
+print (f'multiplicacion= {multiplicacion}')
+print (f'division= {division}')
