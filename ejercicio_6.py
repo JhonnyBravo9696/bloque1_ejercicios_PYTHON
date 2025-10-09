67b4ed1ba13edff49cb66f861e9584b704cf5030
JhonnyBravo9696 <jesuscortesnavarro1705@gmail.com>
2025-10-09 10:02:31 +0200

    Create ejercicio_6.py

---
+++ ejercicio_6.py
@@ -0,0 +1,9 @@
+Numero1 = float(input("introduce el primer Numero:"))
+Numero2 = float(input("introduce el segundo Numero:"))
+Numero3 = float(input("introduce el tercer Numero:"))
+
+suma = Numero1 + Numero2 + Numero3
+
+media = suma / 3
+
+print(f'media de los 3 numeros= {media}')
